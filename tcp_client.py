6e70d7418555ea346750aae22b83ea6cf65e90a3
Darya177777 <57669273+Darya177777@users.noreply.github.com>
2023-10-02 11:20:58 +0700

    Added client

---
+++ tcp_client.py
@@ -0,0 +1,34 @@
+import socket
+from sys import argv
+import os
+
+megabyte = 1024 * 1024
+
+
+def client():
+    script, addr, hostname, port = argv
+    client = socket.socket()
+    client.connect((hostname, int(port)))
+    send_addr = "0" * (4096 - len(addr)) + addr
+    client.send(send_addr.encode())
+    data = client.recv(4096)
+    file_size = os.path.getsize(addr)
+    send_size = "0" * (13 - len(str(file_size))) + str(file_size)
+    client.send(send_size.encode())
+    data = client.recv(13)
+    with open(addr, "r") as f:
+        n = file_size // megabyte
+        for i in range(n):
+            message = f.read(megabyte)
+            client.send(message.encode())
+            data = client.recv(megabyte)
+        if file_size % megabyte != 0:
+            message = f.read(file_size % megabyte)
+            client.send(message.encode())
+            data = client.recv(megabyte)
+        print("Server sent all")
+        client.close()
+
+
+if __name__ == "__main__":
+    client()
